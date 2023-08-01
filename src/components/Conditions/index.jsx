cc6c369386a292e8a0f647c8efbc1d9bae8a5ecf
zhaoma <zhaoma@gmail.com>
2023-08-02 06:55:58 +0800

    any

---
+++ src/components/Conditions/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Conditions extends Component{
+    constructor(props){
+
+    }
+
+    render(){
+        
+    }
+}
---
+++ src/components/Stocks/index.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Stocks extends Component{
+    constructor(props){
+
+    }
+
+    render(){
+        
+    }
+}
